726594cbab8dd63fcb0809346b6fcf12d9107583
MaggyMoller <maggy11@ru.is>
2015-10-21 23:34:52 +0000

    Prints out 3x3 board. No values yet

--- src/is/ru/bazinga/Tic.java
+++ src/is/ru/bazinga/Tic.java
@@ -1,5 +1,26 @@
 package is.ru.bazinga;
 
 public class Tic {
+
+public static final int ROWS = 3;
+public static final int COLUMNS = 3;
+public static int [][] gameBoard = new int [ROWS][COLUMNS];
+
+public static void printGameBoard()
+{
+	for(int i = 0 ; i < ROWS ; i++)
+	{
+		for(int j = 0 ; j < COLUMNS ; j++)
+		{ 
+			System.out.print(" H ");
+		}
+	}
+
+
+}
+
+
+
+
 }
 
